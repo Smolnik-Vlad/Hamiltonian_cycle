fd54f80cd8a8bb3fa5952169ce24d864932d8363
VLADICUS <smolnikvladislav@gmail.com>
2021-11-28 00:39:50 +0300

    azazaza34

---
+++ Hamiltonian cycle/Hamiltonian cycle.cpp
@@ -0,0 +1,96 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+bool cycle_find(int **matrix, int x, int y, int step, vector<int> way, int vertex,  vector<int>& result)
+{
+	way.push_back(vertex);
+	if (step == x-1)
+	{
+		result = way;
+		result.push_back(way[0]);
+		for (int i = 0; i < y; i++) {
+			if (matrix[i][way[0]] == 1 && matrix[i][vertex] == 1) 
+				{
+
+					return true;
+				}
+		}
+
+		return false;
+		
+	}
+	vector<int> child_vertex;
+	for (int i = 0; i < y; i++)
+	{
+		if (matrix[i][vertex] != 0)
+		{
+			for (int j = 0; j < x; j++)
+			{
+				if (j != vertex && matrix[i][j]!=0 && find(way.begin(), way.end(), j)==way.end())
+				{
+					child_vertex.push_back(j);
+					break;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < child_vertex.size(); i++)
+	{
+		bool success = cycle_find(matrix, x, y, step + 1, way, child_vertex[i], result);
+		if (success == 1)
+		{
+			return (1);
+		}
+	}
+	return false;
+
+};
+
+
+int main()
+{
+	ifstream fileo;
+	fileo.open("input.txt");
+	int size_y, size_x;
+	fileo >> size_y; fileo >> size_x;
+
+	int** inc_matrix = new int* [size_y];
+	for (int i = 0; i < size_y; i++)
+	{
+		inc_matrix[i] = new int[size_x];
+	}
+
+	for (int i = 0; i < size_y; i++)
+	{
+		for (int j = 0; j < size_x; j++)
+		{
+			fileo >> inc_matrix[i][j];
+		}
+	}
+	fileo.close();
+
+	vector<int> result;
+	if (cycle_find(inc_matrix, size_x, size_y, 0, vector<int>(), 0, result))
+	{
+		for (int i = 0; i < size_x + 1; i++)
+		{
+			cout << result[i] ;
+			if (i != size_x ) {
+				cout << "->";
+			}
+		}
+	}
+	else cout << "Fu*k";
+
+	for (int i = 0; i < size_y; i++) {
+		delete[] inc_matrix[i];
+	}
+
+	delete[] inc_matrix;
+}
