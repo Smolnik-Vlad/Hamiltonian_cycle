b378ef7eeccb488b3715c0f22543b8ba78a9f93c
Smolnik_Vlad <smolnikvladislav@gmail.com>
2022-04-27 00:46:54 +0300

    sag

--- Hamiltonian cycle.cpp
+++ Hamiltonian cycle.cpp
@@ -5,33 +5,55 @@
 #include <queue>
 using namespace std;
 
-
-bool cycle_find(int **matrix, int x, int y, int step, vector<int> way, int vertex,  vector<int>& result)
+int** get_info(int& nodes, int& edges)   //(ребра и вершины)
 {
-	way.push_back(vertex);
-	if (step == x-1)
+	ifstream fileo;
+	fileo.open("input.txt");
+	fileo >> nodes >> edges;
+
+	int** inc_matrix = new int* [nodes];
+	for (int i = 0; i < nodes; i++)
+	{
+		inc_matrix[i] = new int[edges];
+	}
+
+	for (int i = 0; i < nodes; i++)
+	{
+		for (int j = 0; j < edges; j++)
+		{
+			fileo >> inc_matrix[i][j];
+		}
+	}
+	fileo.close();
+	return inc_matrix;
+}
+
+bool cycle_find(int** matrix, int nodes, int edges, int step, vector<int> way, int vertex, vector<int>& result)
+{
+	way.push_back(vertex); //добавление текущего элемента в цикл
+	if (step == nodes - 1) //Гамильтонов цикл - это такой цикл, в котором проходт по каждой вершине, поэтому шагов будет столько же, сколько и вершин, но так как мы начинаем с шага = 0, то и последний шаг на один меньше
 	{
 		result = way;
-		result.push_back(way[0]);
-		for (int i = 0; i < y; i++) {
-			if (matrix[i][way[0]] != 0 && matrix[i][vertex] != 0) 
-				{
-
-					return true;
-				}
+		result.push_back(way[0]); //добавляем первый элемент для образования цепи
+		for (int i = 0; i < edges; i++) {
+			if (matrix[way[0]][i] != 0 && matrix[vertex][i] != 0)
+			{
+				return true;
+			}
 		}
 
 		return false;
-		
+
 	}
+
 	vector<int> child_vertex;
-	for (int i = 0; i < y; i++)
+	for (int i = 0; i < edges; i++)
 	{
-		if (matrix[i][vertex] != 0)
+		if (matrix[vertex][i] != 0)
 		{
-			for (int j = 0; j < x; j++)
+			for (int j = 0; j < nodes; j++)
 			{
-				if (j != vertex && matrix[i][j]!=0 && find(way.begin(), way.end(), j)==way.end())
+				if (j != vertex && matrix[j][i] != 0 && find(way.begin(), way.end(), j) == way.end())
 				{
 					child_vertex.push_back(j);
 					break;
@@ -42,7 +64,7 @@ bool cycle_find(int **matrix, int x, int y, int step, vector<int> way, int verte
 
 	for (int i = 0; i < child_vertex.size(); i++)
 	{
-		bool success = cycle_find(matrix, x, y, step + 1, way, child_vertex[i], result);
+		bool success = cycle_find(matrix, nodes, edges, step + 1, way, child_vertex[i], result);
 		if (success == 1)
 		{
 			return (1);
@@ -55,40 +77,24 @@ bool cycle_find(int **matrix, int x, int y, int step, vector<int> way, int verte
 
 int main()
 {
-	ifstream fileo;
-	fileo.open("input.txt");
-	int size_y, size_x;
-	fileo >> size_y; fileo >> size_x;
+	int edges, nodes;
+	int** inc_matrix = get_info(nodes, edges);
 
-	int** inc_matrix = new int* [size_y];
-	for (int i = 0; i < size_y; i++)
-	{
-		inc_matrix[i] = new int[size_x];
-	}
-
-	for (int i = 0; i < size_y; i++)
-	{
-		for (int j = 0; j < size_x; j++)
-		{
-			fileo >> inc_matrix[i][j];
-		}
-	}
-	fileo.close();
 
 	vector<int> result;
-	if (cycle_find(inc_matrix, size_x, size_y, 0, vector<int>(), 0, result))
+	if (cycle_find(inc_matrix, nodes, edges, 0, vector<int>(), 0, result))
 	{
-		for (int i = 0; i < size_x + 1; i++)
+		for (int i = 0; i < result.size(); i++)
 		{
-			cout << result[i] ;
-			if (i != size_x ) {
+			cout << result[i];
+			if (i != result.size() - 1) {
 				cout << "->";
 			}
 		}
 	}
 	else cout << "Fu*k";
 
-	for (int i = 0; i < size_y; i++) {
+	for (int i = 0; i < nodes; i++) {
 		delete[] inc_matrix[i];
 	}
 
